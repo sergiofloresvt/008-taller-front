c72cdc1ebeab7e0bdf961b8060377566e6fffbe3
chaliexe <lazaronfischer@gmail.com>
2023-11-27 21:07:49 -0300

    facturaccion
    se importa la libreria de angular materia forms y se realiza las declaraciones correspondientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { ContactComponent } from "./components/pages/contact/contact.component";
 
 const routes : Routes =
 [
-   { path:'home', component:HomeComponent},
+   { path:'home', component:InvoicesComponent},
 //    {path:'client', component:ClientComponent,
 // children:[
 //    {
